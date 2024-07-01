3e653b53880b2cb646b2fd0a09448e2f5ccd74c0
ibraah Adams <161978665+kingibrahimtech@users.noreply.github.com>
2024-07-01 17:00:11 +0300

    black project 👹

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -65,6 +65,212 @@ async (origineMessage, zk, commandeOptions) => {
   }
 });
 
+
+zokou({
+  nomCom: "blackpink",
+  categorie: "Hentai",
+  reaction: "🛸"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.maher-zubair.tech/wallpaper/blackpink'//apiWaifu("neko"); // Remplace avec ton lien réel
+
+  try { for (let i = 0 ;i < 5 ; i++) {
+    const response = await axios.get(url);
+    const imageUrl = response.data.url;
+
+    zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms }); }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+
+zokou({
+  nomCom: "car",
+  categorie: "Hentai",
+  reaction: "🚘"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.maher-zubair.tech/wallpaper/car'//apiWaifu("neko"); // Remplace avec ton lien réel
+
+  try { for (let i = 0 ;i < 5 ; i++) {
+    const response = await axios.get(url);
+    const imageUrl = response.data.url;
+
+    zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms }); }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+zokou({
+  nomCom: "wallpaper",
+  categorie: "Hentai",
+  reaction: "🎞"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.maher-zubair.tech/wallpaper/random'//apiWaifu("neko"); // Remplace avec ton lien réel
+
+  try { for (let i = 0 ;i < 5 ; i++) {
+    const response = await axios.get(url);
+    const imageUrl = response.data.url;
+
+    zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms }); }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+zokou({
+  nomCom: "pubg",
+  categorie: "Hentai",
+  reaction: "💥"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.maher-zubair.tech/wallpaper/pubg'//apiWaifu("neko"); // Remplace avec ton lien réel
+
+  try { for (let i = 0 ;i < 5 ; i++) {
+    const response = await axios.get(url);
+    const imageUrl = response.data.url;
+
+    zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms }); }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+zokou({
+  nomCom: "mlegend",
+  categorie: "Hentai",
+  reaction: "🤭"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.maher-zubair.tech/wallpaper/mlegend'//apiWaifu("neko"); // Remplace avec ton lien réel
+
+  try { for (let i = 0 ;i < 5 ; i++) {
+    const response = await axios.get(url);
+    const imageUrl = response.data.url;
+
+    zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms }); }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+
+zokou({
+  nomCom: "messi",
+  categorie: "Hentai",
+  reaction: "🔘"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.maher-zubair.tech/wallpaper/messi'//apiWaifu("neko"); // Remplace avec ton lien réel
+
+  try { for (let i = 0 ;i < 5 ; i++) {
+    const response = await axios.get(url);
+    const imageUrl = response.data.url;
+
+    zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms }); }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+    
+zokou({
+  nomCom: "dog",
+  categorie: "Hentai",
+  reaction: "🐯"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.maher-zubair.tech/wallpaper/dog'//apiWaifu("neko"); // Remplace avec ton lien réel
+
+  try { for (let i = 0 ;i < 5 ; i++) {
+    const response = await axios.get(url);
+    const imageUrl = response.data.url;
+
+    zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms }); }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+zokou({
+  nomCom: "cat",
+  categorie: "Hentai",
+  reaction: "🐯"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.maher-zubair.tech/wallpaper/cat'//apiWaifu("neko"); // Remplace avec ton lien réel
+
+  try { for (let i = 0 ;i < 5 ; i++) {
+    const response = await axios.get(url);
+    const imageUrl = response.data.url;
+
+    zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms }); }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+
+zokou({
+  nomCom: "cr7",
+  categorie: "Hentai",
+  reaction: "🌍"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.maher-zubair.tech/wallpaper/cr7'//apiWaifu("neko"); // Remplace avec ton lien réel
+
+  try { for (let i = 0 ;i < 5 ; i++) {
+    const response = await axios.get(url);
+    const imageUrl = response.data.url;
+
+    zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms }); }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
+
+zokou({
+  nomCom: "bike",
+  categorie: "Hentai",
+  reaction: "🏳"
+},
+async (origineMessage, zk, commandeOptions) => {
+  const { repondre, ms } = commandeOptions;
+
+  const url = 'https://api.maher-zubair.tech/wallpaper/bike'//apiWaifu("neko"); // Remplace avec ton lien réel
+
+  try { for (let i = 0 ;i < 5 ; i++) {
+    const response = await axios.get(url);
+    const imageUrl = response.data.url;
+
+    zk.sendMessage(origineMessage, { image: { url: imageUrl } }, { quoted: ms }); }
+  } catch (error) {
+    repondre('Erreur lors de la récupération des données :', error);
+  }
+});
+
 zokou({
   nomCom: "hneko",
   categorie: "Hentai",
