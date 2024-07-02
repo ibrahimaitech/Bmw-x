9eaf821cb95f2dc09b819056cada59a868e19645
ibraah Adams <161978665+kingibrahimtech@users.noreply.github.com>
2024-07-02 22:34:23 +0300

    black project 👹

--- plugins/Answer.js
+++ plugins/Answer.js
@@ -44,43 +44,6 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
   });  
 
 
-zokou({nomCom:"ai",reaction:"📡",categorie:"IA"},async(dest,zk,commandeOptions)=>{
-
-  const {repondre,ms,arg}=commandeOptions;
-  
-    if(!arg || !arg[0])
-    {return repondre("yes I'm listening to you.")}
-    //var quest = arg.join(' ');
-  try{
-    
-    
-const message = await traduire(arg.join(' '),{ to : 'en'});
- console.log(message)
-fetch(`https://gpt4.giftedtech.workers.dev/?prompt=Hello,%20Are%20you%20Gpt-4=${message}`)
-.then(response => response.json())
-.then(data => {
-  const botResponse = data.cnt;
-  console.log(botResponse);
-
-  traduire(botResponse, { to: 'en' })
-    .then(translatedResponse => {
-      repondre(translatedResponse);
-    })
-    .catch(error => {
-      console.error('Error when translating into French :', error);
-      repondre('Error when translating into French');
-    });
-})
-.catch(error => {
-  console.error('Error requesting BrainShop :', error);
-  repondre('Error requesting BrainShop');
-});
-
-  }catch(e){ repondre("oops an error : "+e)}
-    
-  
-  });  
-
 
   zokou({ nomCom: "dalle", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
@@ -121,6 +84,31 @@ fetch(`https://gpt4.giftedtech.workers.dev/?prompt=Hello,%20Are%20you%20Gpt-4=${
       // Regrouper les arguments en une seule chaîne séparée par "-"
       const question = arg.join(' ');
       const response = await axios.get(`http://api.maher-zubair.tech/ai/chatgpt4?q=${question}`);
+      
+      const data = response.data;
+      if (data) {
+        repondre(data.result);
+      } else {
+        repondre("Error during response generation.");
+      }
+    } catch (error) {
+      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+      repondre("Oops, an error occurred while processing your request.");
+    }
+  });
+
+
+zokou({ nomCom: "ai", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+  
+    try {
+      if (!arg || arg.length === 0) {
+        return repondre(`Please ask a question.`);
+      }
+  
+      // Regrouper les arguments en une seule chaîne séparée par "-"
+      const question = arg.join(' ');
+      const response = await axios.get(`https://gpt4.giftedtech.workers.dev/?prompt=Hello,%20Are%20you%20Gpt-4?q=${question}`);
       
       const data = response.data;
       if (data) {
