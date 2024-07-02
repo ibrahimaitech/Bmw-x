f0bd81e445dda1bc341bb30285f5c5f1cb7c5f05
ibraah Adams <161978665+kingibrahimtech@users.noreply.github.com>
2024-07-03 00:51:55 +0300

    black project 👹

--- plugins/parole.js
+++ plugins/parole.js
@@ -2,7 +2,7 @@ const {zokou} =require("../framework/zokou");
 const axios =require("axios");
 
 
-zokou({ nomCom: "lyrics",
+zokou({ nomCom: "meo",
         reaction: "✨",
         categorie: "Search" }, async (dest, zk, commandeOptions) => {
     
@@ -34,3 +34,28 @@ ${lyrics.lyrics}`
        repondre('Error')
    }
         })
+
+zokuo({
+  'nomCom': "lyrics",
+  'reaction': '✨',
+  'categorie': "Search"
+}, async (dest, zk, commandeOptions) => {
+  const {
+    const { repondre, arg, ms } = commandeOptions;;
+  try {
+    if (!arg || arg.length === 0) return repondre("Where is the name of song");
+    }
+    const _0x2d6993 = _0x47ee56.join(" ");
+    const _0x19a972 = await Client.songs.search(_0x2d6993);
+    const _0x349a1c = _0x19a972[0x0];
+    const _0x3e8204 = await _0x349a1c.lyrics();
+    await _0x24921b.sendMessage(_0x16b585, {
+      'text': _0x3e8204
+    }, {
+      'quoted': _0x26dbd3
+    });
+  } catch (err) {
+    reply("I did not find any lyrics for " + text + ". Try searching a different song.");
+    console.log(_0xe736b5);
+  }
+});
