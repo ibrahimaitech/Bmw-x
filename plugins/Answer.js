2d69f76eb8e3cbbc3ef275fe20b791c79261cfb0
ibraah Adams <161978665+kingibrahimtech@users.noreply.github.com>
2024-07-03 03:00:27 +0300

    black project 👹

--- plugins/Answer.js
+++ plugins/Answer.js
@@ -122,29 +122,5 @@ zokou({ nomCom: "gpt", reaction: "🤔", categorie: "IA" }, async (dest, zk, com
     }
   });
 
-zokou({ nomCom: "lyrics", reaction: "📂", categorie: "IA" }, async (dest, zk, commandeOptions) => {
-    const { repondre, arg, ms } = commandeOptions;
-  
-    try {
-      if (!arg || arg.length === 0) {
-        return repondre(`Give me the name of the song.`);
-      }
-  
-      // Regrouper les arguments en une seule chaîne séparée par "-"
-      const question = arg.join(' ');
-      const response = await axios.get(`https://api.maher-zubair.tech/search/lyrics?q=${question}`);
-      
-      const data = response.data;
-      if (data) {
-        repondre(data.result);
-      } else {
-        repondre("Error during response generation.");
-      }
-    } catch (error) {
-      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
-      repondre("Oops, an error occurred while processing your request.");
-    }
-  });
-
 
   
